dadcd322fe0d5f0b0174e27067e3726f0810c6e7
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-11-02 23:54:06 +0200

    upload

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 30) {
       playing = false;
       diceEl.classList.add('hidden');
       document
